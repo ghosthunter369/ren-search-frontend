dc465cf26f0f4c17a75a66d0fda9d069a8059507
Stefanie Sun Fans <14032262+stefanie-sun-fans@user.noreply.gitee.com>
2025-03-30 20:40:55 +0800

    完成单个页面按类型查询

--- src/pages/IndexView.vue
+++ src/pages/IndexView.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="home">
     <a-input-search
-      v-model:value="searchParams.text"
+      v-model:value="searchParams.searchText"
       placeholder="请输入查询内容"
       enter-button="搜索"
       size="large"
       @search="onSearch"
     />
     <a-tabs v-model:activeKey="activeKey" @change="onTaChange">
-      <a-tab-pane key="1" tab="图片"
-        ><picture-list :pictureList="pictureList"
-      /></a-tab-pane>
-      <a-tab-pane key="2" tab="帖子">
+      <a-tab-pane key="picture" tab="图片">
+        <picture-list :pictureList="pictureList" />
+      </a-tab-pane>
+      <a-tab-pane key="post" tab="帖子">
         <post-list :postList="postList" />
       </a-tab-pane>
-      <a-tab-pane key="3" tab="用户">
+      <a-tab-pane key="user" tab="用户">
         <user-list :userList="userList" />
       </a-tab-pane>
     </a-tabs>
@@ -32,46 +32,65 @@ import myAxios from "@/plugins/myAxios";
 const router = useRouter();
 const route = useRoute();
 
-const activeKey = ref(route.params.category || "1");
+const activeKey = ref(route.params.category || "post");
 
 const searchParams = ref({
-  text: "",
+  searchText: "忍野忍", // 修改字段名
   pageSize: 10,
   pageNum: 1,
+  type: activeKey.value,
 });
+
 const postList = ref([]);
 const userList = ref([]);
-const pictureList = ref([]); // 使用 markRaw 标记为非响应式对象
+const pictureList = ref([]);
 
-myAxios.post("/post/list/page/vo", {}).then(function (res: any) {
-  postList.value = res.records || []; // 确保有数据
-});
-myAxios.post("/user/list/page/vo", {}).then(function (res: any) {
-  userList.value = res.records || []; // 确保有数据
-});
-myAxios.post("picture/list/page/vo", {}).then((res: any) => {
-  pictureList.value = res.records || [];
-});
-watchEffect(() => {
-  Object.assign(searchParams.value, {
-    text: (route.query.text as string) || "",
-  });
-});
-
-const onSearch = (searchText: string) => {
-  router.push({
-    query: {
-      text: searchText,
-    },
+const loadDataNew = (params: any) => {
+  myAxios.post("/search/get/all", params).then((res: any) => {
+    switch (params.type) {
+      case "post":
+        postList.value = res.postVOS || [];
+        break;
+      case "user":
+        userList.value = res.userVOS || [];
+        break;
+      case "picture":
+        pictureList.value = res.pictures || [];
+        break;
+    }
   });
 };
 
+// 监听路由变化，更新搜索参数
+watchEffect(() => {
+  searchParams.value.searchText = (route.query.searchText as string) || "";
+});
+
+// 搜索事件
+const onSearch = (searchText: string) => {
+  router.push({
+    query: {
+      searchText: searchText, // 确保 URL 参数名也修改
+    },
+  });
+
+  searchParams.value.searchText = searchText;
+  loadDataNew(searchParams.value);
+};
+
+// 切换 Tab 事件
 const onTaChange = (key: string) => {
   router.push({
     path: `/${key}`,
     query: {
-      text: searchParams.value.text,
+      searchText: searchParams.value.searchText, // 确保 URL 参数名也修改
     },
   });
+
+  searchParams.value.type = key;
+  loadDataNew(searchParams.value);
 };
+
+// 初始化数据
+loadDataNew(searchParams.value);
 </script>
